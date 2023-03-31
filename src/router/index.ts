eea544ddda523d003612b6ed15086ca5d4f0bc16
ce-amtic <fyh516@126.com>
2023-03-31 09:02:16 +0800

    deploy config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import Home from "~/view/Home.vue"
 import About from "~/view/About.vue"
 import Layout from "~/view/Layout.vue"
@@ -12,7 +12,8 @@ const routes = [
   ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    // history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
